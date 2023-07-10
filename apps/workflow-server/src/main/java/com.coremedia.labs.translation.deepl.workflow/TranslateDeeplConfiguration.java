99ce162d4c60d713f61cad18417f510f640586b1
Sebastian <sebastian.buettner@coremedia.com>
2023-07-10 13:49:55 +0200

    Adding spring config for deepl extension

---
+++ apps/workflow-server/src/main/java/com.coremedia.labs.translation.deepl.workflow/TranslateDeeplConfiguration.java
@@ -0,0 +1,45 @@
+package com.coremedia.labs.translation.deepl.workflow;
+
+import com.coremedia.cap.translate.xliff.config.XliffExporterConfiguration;
+import com.coremedia.cap.translate.xliff.config.XliffImporterConfiguration;
+import com.coremedia.translate.item.TranslateItemConfiguration;
+import com.coremedia.translate.workflow.DefaultTranslationWorkflowDerivedContentsStrategy;
+import com.coremedia.translate.workflow.TranslationWorkflowDerivedContentsStrategy;
+import edu.umd.cs.findbugs.annotations.DefaultAnnotation;
+import edu.umd.cs.findbugs.annotations.NonNull;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@Import({
+        XliffImporterConfiguration.class,
+        XliffExporterConfiguration.class,
+        TranslateItemConfiguration.class})
+@PropertySource(value = "classpath:META-INF/coremedia/deepl-workflow.properties")
+@DefaultAnnotation(NonNull.class)
+public class TranslateDeeplConfiguration {
+
+  /**
+   * A strategy for extracting derived contents from the default translation.xml workflow definition.
+   *
+   * @return deeplTranslationWorkflowDerivedContentsStrategy
+   */
+  @Bean
+  TranslationWorkflowDerivedContentsStrategy deeplTranslationWorkflowDerivedContentsStrategy(){
+    DefaultTranslationWorkflowDerivedContentsStrategy deeplTranslationWorkflowDerivedContentsStrategy = new DefaultTranslationWorkflowDerivedContentsStrategy();
+    deeplTranslationWorkflowDerivedContentsStrategy.setProcessDefinitionName("TranslationDeepl");
+    return deeplTranslationWorkflowDerivedContentsStrategy;
+  }
+
+  @ConfigurationProperties(prefix = "deepl")
+  @Bean
+  public Map<String, Object> deeplConfigurationProperties() {
+    return new HashMap<>();
+  }
+}
