9c6e7ecc9a21793cf3fb06edd07fec141a595a7b
Sebastian <sebastian.buettner@coremedia.com>
2023-07-11 16:22:23 +0200

    Adding transitions, state labels and icon

--- apps/studio-client/shared/ext/deepl/src/DeeplWorkflowPlugin.ts
+++ apps/studio-client/shared/ext/deepl/src/DeeplWorkflowPlugin.ts
@@ -1,6 +1,7 @@
 import { workflowLocalizationRegistry } from "@coremedia/studio-client.workflow-plugin-models/WorkflowLocalizationRegistry";
 import { workflowPlugins } from "@coremedia/studio-client.workflow-plugin-models/WorkflowPluginRegistry";
 import Deepl_properties from "./Deepl_properties";
+import deeplWorkflowIcon from "./icons/deepl-workflow.svg";
 
 const WORKFLOW_NAME:string = "TranslationDeepl";
 
@@ -12,11 +13,34 @@ workflowPlugins._.addTranslationWorkflowPlugin<DeeplViewModel>({
   workflowType: "TRANSLATION",
   workflowName: WORKFLOW_NAME,
   createWorkflowPerTargetSite: false,
+  nextStepVariable: "translationAction",
+  transitions: [
+    {
+      task: "Review",
+      defaultNextTask: "finishTranslation",
+      nextSteps: [
+        {
+          name: "rollbackTranslation",
+          allowAlways: true,
+        },
+        {
+          name: "finishTranslation",
+          allowAlways: true,
+        },
+      ],
+    },
+  ],
+
 });
 
 workflowLocalizationRegistry._.addLocalization(WORKFLOW_NAME, {
   displayName: Deepl_properties.TranslationDeepl_displayName,
   description: Deepl_properties.TranslationDeepl_description,
+  svgIcon: deeplWorkflowIcon,
+  states: {
+    finishTranslation: Deepl_properties.TranslationDeepl_state_finishTranslation_displayName,
+    rollbackTranslation: Deepl_properties.TranslationDeepl_state_rollbackTranslation_displayName
+  }
 });
 
 workflowLocalizationRegistry._.addIssuesLocalization({
--- apps/studio-client/shared/ext/deepl/src/Deepl_properties.ts
+++ apps/studio-client/shared/ext/deepl/src/Deepl_properties.ts
@@ -1,6 +1,8 @@
 interface Deepl_properties {
   TranslationDeepl_displayName: string,
   TranslationDeepl_description: string,
+  TranslationDeepl_state_rollbackTranslation_displayName: string,
+  TranslationDeepl_state_finishTranslation_displayName: string,
 
   SUCCESS_singular_text: string,
   SUCCESS_plural_text: string,
@@ -9,7 +11,8 @@ interface Deepl_properties {
 const Deepl_properties : Deepl_properties = {
   TranslationDeepl_description: "Translation with DeepL",
   TranslationDeepl_displayName: "Translation with DeepL",
-
+  TranslationDeepl_state_rollbackTranslation_displayName: "Reject changes",
+  TranslationDeepl_state_finishTranslation_displayName: "Finish content Localization",
   SUCCESS_singular_text: "The translation result has successfully been imported.",
   SUCCESS_plural_text: "The translation results have successfully been imported.",
 }
