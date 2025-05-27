95ea59fc46902e11b936e75d30d4649edce56cdc
jlinke <jan-h.linke@gmx.de>
2025-05-27 17:29:40 +0200

    - Fixing or deactivating broken Unit Tests

--- apps/studio-server/src/test/java/com/coremedia/labs/translation/deepl/studio/validation/DeeplSupportedLanguagesValidatorTest.java
+++ apps/studio-server/src/test/java/com/coremedia/labs/translation/deepl/studio/validation/DeeplSupportedLanguagesValidatorTest.java
@@ -4,7 +4,6 @@ package com.coremedia.labs.translation.deepl.studio.validation;
 import com.coremedia.cap.multisite.SitesService;
 import com.deepl.api.DeepLException;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -29,31 +28,29 @@ class DeeplSupportedLanguagesValidatorTest {
   void testIsValidLocale() {
     assertTrue(testling.isValidLocale(Locale.GERMANY, List.of(Locale.US, Locale.UK, Locale.GERMAN, Locale.GERMANY)));
   }
-
+/*
   @Test
-  @Disabled
   void testIsValidLocaleList() throws DeepLException, InterruptedException {
     List<Locale> localesToCheck = List.of(Locale.GERMANY, Locale.ITALY, Locale.FRANCE);
     List<Locale> validLocales = List.of(Locale.FRENCH, Locale.ITALIAN, Locale.GERMAN);
     assertTrue(testling.isValidLocaleList(localesToCheck, testling.getSupportedSourceLocales()));
     assertFalse(testling.isValidLocaleList(List.of(Locale.UK, Locale.GERMANY), validLocales));
-  }
+  }*/
 
 
 
 
-  @Test
-  @Disabled
+/*  @Test
   void testGetSupportedSourceLocales() throws DeepLException, InterruptedException {
     List<Locale> SUPPORTED_SOURCE_LOCALES = Stream.of("BG", "CS", "DA", "DE", "EL", "EN", "ES", "ET", "FI", "FR", "HU", "ID", "IT", "JA", "KO", "LT", "LV", "NB", "NL", "PL", "PT", "RO", "RU", "SK", "SL", "SV", "TR", "UK", "ZH").map(Locale::forLanguageTag).collect(Collectors.toList());
     assertEquals(SUPPORTED_SOURCE_LOCALES, testling.getSupportedSourceLocales());
   }
+
   @Test
-  @Disabled
   void testGetSupportedTargetLocales() throws DeepLException, InterruptedException {
     List<Locale> SUPPORTED_TARGET_LOCALES = Stream.of("BG", "CS", "DA", "DE", "EL", "EN", "EN-GB", "EN-US", "ES", "ET", "FI", "FR", "HU", "ID", "IT", "JA", "KO", "LT", "LV", "NB", "NL", "PL", "PT", "PT-BR", "PT-PT", "RO", "RU", "SK", "SL", "SV", "TR", "UK", "ZH").map(Locale::forLanguageTag).collect(Collectors.toList());
     assertEquals(SUPPORTED_TARGET_LOCALES, testling.getSupportedTargetLocales());
-  }
+  }*/
 
   @Test
   void testGetFirstInvalidLocale() {
@@ -63,11 +60,9 @@ class DeeplSupportedLanguagesValidatorTest {
 
   }
   @Test
-  @Disabled
   void testValidLocales() {
     List<Locale> localesToCheck = List.of(Locale.GERMANY,Locale.ITALY, Locale.FRANCE);
     List<Locale> validLocales = List.of(Locale.FRENCH, Locale.ITALIAN, Locale.GERMAN);
-    assertNull(testling.getFirstInvalidLocale(localesToCheck, validLocales));
-
+    assertEquals(testling.getFirstInvalidLocale(localesToCheck, validLocales), Optional.empty());
   }
 }
