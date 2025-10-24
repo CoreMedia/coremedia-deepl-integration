457f471c6cf91ae58966b6e25a21455bd54eba3b
Philip Hemptenmacher <philip.hemptenmacher@coremedia.com>
2025-10-24 16:11:44 +0200

    SALESDEMO-3502: Updated german translations

--- apps/studio-client/shared/ext/deepl/src/Deepl_de_properties.ts
+++ apps/studio-client/shared/ext/deepl/src/Deepl_de_properties.ts
@@ -4,8 +4,8 @@ import Deepl_properties from "./Deepl_properties";
 ResourceBundleUtil.override(Deepl_properties, {
   TranslationDeepl_description: "Übersetzung mit DeepL mit Abnahme",
   TranslationDeepl_displayName: "Übersetzung mit DeepL mit Abnahme",
-  TranslationDeeplDirect_displayName: "Übersetzung mit DeepL",
-  TranslationDeeplDirect_description: "Übersetzung mit DeepL",
+  TranslationDeeplDirect_displayName: "Direkte AI Übersetzung mit DeepL",
+  TranslationDeeplDirect_description: "Direkte AI Übersetzung mit DeepL",
   TranslationDeepl_field_createProject_label: "Projekt erstellen",
   TranslationDeepl_field_createProject_tooltip: "Erstellt ein Projekt, mit allen geänderten Inhalten, wenn der Workflow beendet ist",
   SUCCESS_singular_text: "Das Übersetzungsergebnis wurde erfolgreich importiert.",
