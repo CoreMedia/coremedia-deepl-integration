842cfb6875ebcf7fec673767aa5d7d8df1fd1318
Philip Hemptenmacher <philip.hemptenmacher@coremedia.com>
2026-01-20 13:17:28 +0100

    Added WfValidators for DeepLinkDirect workflow

--- apps/studio-server/src/main/java/com/coremedia/labs/translation/deepl/studio/validation/DeeplWorkflowValidationAutoConfiguration.java
+++ apps/studio-server/src/main/java/com/coremedia/labs/translation/deepl/studio/validation/DeeplWorkflowValidationAutoConfiguration.java
@@ -59,4 +59,31 @@ public class DeeplWorkflowValidationAutoConfiguration {
 
     return new WorkflowValidatorsModel(TRANSLATION_DEEPL_VALIDATOR_KEY, taskValidators, deeplStartValidators);
   }
+
+
+  @Bean
+  WorkflowValidatorsModel translationDeeplDirectWFValidators(@Qualifier(TRANSLATION_START_VALIDATORS) WorkflowStartValidators translationStartValidators,
+                                                             @Qualifier(TRANSLATION_WFNOT_RUNNING) List<WorkflowValidator> translationWFNotRunning,
+                                                             @Qualifier(TRANSLATION_WFRUNNING) List<WorkflowValidator> translationWFRunning,
+                                                             @Qualifier(TASK_ERROR_VALIDATOR) WorkflowValidator taskErrorValidator,
+                                                             DeeplConfigurationService deeplConfigurationService,
+                                                             SitesService sitesService) {
+    ValidationTask runningTask = new ValidationTask(TRANSLATE_TASK_NAME, TaskState.RUNNING);
+    ValidationTask waitingTask = new ValidationTask(TRANSLATE_TASK_NAME, TaskState.ACTIVATED);
+
+    WorkflowTaskValidators taskValidators = new WorkflowTaskValidators(
+            Map.of(runningTask, translationWFRunning,
+                    waitingTask, translationWFNotRunning
+            )
+    );
+
+    List<WorkflowValidator> workflowValidators = new ArrayList<>();
+    workflowValidators.add(new DeeplSupportedLanguagesValidator(deeplConfigurationService, sitesService));
+    workflowValidators.addAll(translationStartValidators.getWorkflowValidators());
+    WorkflowStartValidators deeplStartValidators = new WorkflowStartValidators(
+            translationStartValidators.getWorkflowValidationPreparation(), workflowValidators
+    );
+
+    return new WorkflowValidatorsModel(TRANSLATION_DEEPL_DIRECT_VALIDATOR_KEY, taskValidators, deeplStartValidators);
+  }
 }
